3ca7f69a8aada58822a9fb56b2753e7b3c5cbeb0
Ricard Catalan <ricard.catalan@gmail.com>
2016-06-22 13:19:09 +0200

    Added overwrite basic CSS II

--- game.php
+++ game.php
@@ -13,22 +13,28 @@ $output = Automate::factory()->getGame($url.'?'.$_SERVER['QUERY_STRING'], $_POST
 <!DOCTYPE html>
 <html lang="en">
   <head>
-  <meta name="viewport" content="width=device-width, user-scalable=no">
+      <meta name="viewport" content="width=device-width, user-scalable=yes">
       <meta http-equiv="X-UA-Compatible" content="IE=EDGE" />
       <meta http-equiv="Content-type" content="text/html;charset=UTF-8"/>
       <title>Game</title>
   </head>
   <style type="text/css">
     * {
-      background-color: #222;
+      background-color: #111;
       background-image: none;
+      color:#ccc;
+      font-size:11px;
     }
     img {
       opacity: 0.4;
     }
 
     a, a:visited, a:link {
-      color: #ccc !important;
+      color: #eee;
+      text-decoration: underline;
+    }
+    .quickstart_pane a {
+      font-size:12px;
     }
   </style>
   <body style="background-color: #444;">
