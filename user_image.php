b7e60e09a6d2bab8924e54fae434d01759b4294e
Ricard Catalan <zicard@gmail.com>
2017-03-25 23:48:16 +0100

    Create user_image.php

---
+++ user_image.php
@@ -0,0 +1,6 @@
+<?php
+include_once(dirname(__FILE__).'/automate.class.php'); // parent
+$config = Automate::factory()->getConfig();
+$url = "{$config['protocol']}://{$config['server']}.{$config['domain']}/user_image.php";
+echo Automate::factory()->getGame($url.'?'.$_SERVER['QUERY_STRING'], $_POST, true, true);
+?>
